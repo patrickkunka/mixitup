c3b54d77940e1072292ff2ffbd78ee881cf2092e
Patrick Kunka <patrick.kunka@barrelny.com>
2016-09-05 23:29:09 +0100

    add facade constructor

---
+++ src/facade.js
@@ -0,0 +1,49 @@
+/* global mixitup, h */
+
+/**
+ * @constructor
+ * @namespace
+ * @memberof    mixitup
+ * @private
+ * @since       3.0.0
+ * @param       {mixitup.Mixer} mixer
+ */
+
+mixitup.Facade = function Mixer(mixer) {
+    mixitup.Base.call(this);
+
+    this.execAction('construct', 0);
+
+    this.init           = mixer.init.bind(mixer);
+    this.show           = mixer.show.bind(mixer);
+    this.hide           = mixer.hide.bind(mixer);
+    this.filter         = mixer.filter.bind(mixer);
+    this.sort           = mixer.sort.bind(mixer);
+    this.changeLayout   = mixer.changeLayout.bind(mixer);
+    this.multiMix       = mixer.multiMix.bind(mixer);
+    this.tween          = mixer.tween.bind(mixer);
+    this.insert         = mixer.insert.bind(mixer);
+    this.insertBefore   = mixer.insertBefore.bind(mixer);
+    this.insertAfter    = mixer.insertAfter.bind(mixer);
+    this.prepend        = mixer.prepend.bind(mixer);
+    this.append         = mixer.append.bind(mixer);
+    this.remove         = mixer.remove.bind(mixer);
+    this.destroy        = mixer.destroy.bind(mixer);
+    this.forceRefresh   = mixer.forceRefresh.bind(mixer);
+    this.isMixing       = mixer.isMixing.bind(mixer);
+    this.getOperation   = mixer.getOperation.bind(mixer);
+    this.getOption      = mixer.getOption.bind(mixer);
+    this.setOptions     = mixer.setOptions.bind(mixer);
+    this.getState       = mixer.getState.bind(mixer);
+
+    this.execAction('construct', 1);
+
+    h.freeze(this);
+    h.seal(this);
+};
+
+mixitup.BaseStatic.call(mixitup.Facade);
+
+mixitup.Facade.prototype = Object.create(mixitup.Base.prototype);
+
+mixitup.Facade.prototype.constructor = mixitup.Facade;
